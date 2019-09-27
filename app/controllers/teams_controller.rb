75ee7fea7cf1ee0d607ebda8a7cfffc04ede9c75
hidakago <hidakago@gmail.com>
2019-09-27 21:49:05 +0900

    Add:Teamに所属しているUserの削除は、そのTeamのオーナーか、そのUser自身しかできないようにする

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -15,7 +15,11 @@ class TeamsController < ApplicationController
     @team = Team.new
   end
 
-  def edit; end
+  # def edit; end
+def edit
+  puts "*****************************"
+  return
+end
 
   def create
     @team = Team.new(team_params)
